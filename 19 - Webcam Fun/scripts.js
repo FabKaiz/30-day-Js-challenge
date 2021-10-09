dbf81a57692cc560a80498bec9d80b3462c6c701
Fabien Chareun <cacrfabfr@gmail.com>
2021-10-09 18:57:32 +0200

    finished day 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,89 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+const getVideo = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch((error) => {
+      console.error("Error, maybe no webcam :)");
+    });
+};
+
+const paintToCanvas = () => {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+};
+
+const takePhoto = () => {
+  snap.currentTime = 0;
+  snap.play();
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "picture");
+  link.innerHTML = `<img src="${data}" alt="Picture" />`;
+  strip.insertBefore(link, strip.firstChild);
+};
+
+const redEffect = (pixels) => {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // Red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // Green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+};
+
+const rgbSplit = (pixels) => {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // Red
+    pixels.data[i + 500] = pixels.data[i + 1]; // Green
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+};
+
+const greenScreen = (pixels) => {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+};
+
+getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
